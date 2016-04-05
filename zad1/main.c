23d4d3c6792e54f00327ab11e4cdf2fe7d4921b1
19mariusz95 <19mariusz95@gmail.com>
2016-04-05 23:10:14 +0200

    N arg zad1

--- zad1/main.c
+++ zad1/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv){
+int get_N_arg(int argc, char *argv[]);
 
-    printf("Hello World\n");
+int main(int argc, char *argv[]) {
+
+    int N = get_N_arg(argc, argv);
+    printf("%d\n", N);
+}
+
+int get_N_arg(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("trolo");
+        exit(1);
+    }
+    int result = atoi(argv[1]);
+    if (result == 0) {
+        printf("troloolo");
+        exit(1);
+    }
+    return result;
 }
